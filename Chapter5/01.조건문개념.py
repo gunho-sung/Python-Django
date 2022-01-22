1a331439a30684190636500215ebd4dc21aeee88
rjsgh8677 <rjsgh8677@naver.com>
2022-01-22 21:29:24 +0900

    복습 리눅스 환경 셋팅해둬서 오류났음 cmd.exe로 vscode 터미널 기본값 변경함

---
+++ Chapter5/01.조건문개념.py
@@ -0,0 +1,10 @@
+# 조건문
+# : 조건에 따라 실행할 명령이 달라 지는 것
+
+origin_pass = "1234"
+input_pass = input("패스워드를 입력하세요 >>>")
+
+if input_pass == origin_pass: #조건 A 
+    #조건 A 참
+    print("로그인 성공!")
+    print("반갑습니다.")
